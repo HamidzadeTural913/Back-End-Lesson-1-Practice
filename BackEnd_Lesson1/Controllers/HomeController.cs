56ef6ac4a10757d4be710859170c7fab6cca1f16
HamidzadeTural913 <tu76fe6rc@code.edu.az>
2022-05-23 14:54:52 +0400

    Task Added

---
+++ BackEnd_Lesson1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_Lesson1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
